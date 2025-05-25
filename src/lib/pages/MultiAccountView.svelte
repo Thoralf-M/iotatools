04b9813a03677d54537ebae6b37db9c415b10f53
Thoralf Müller <thoralf.mue@gmail.com>
2025-05-25 11:36:38 +0200

    feat(MultiAccountView): add dry run

--- src/lib/pages/MultiAccountView.svelte
+++ src/lib/pages/MultiAccountView.svelte
@@ -17,12 +17,15 @@
         id: string;
         address: string;
         label: string | undefined;
-        objects: {
-            id: string;
-            label: string;
-            data: any;
-            currentOwner: string;
-        }[];
+        objects: ExtendedObject[];
+        timelockedObjects: ExtendedObject[];
+    }
+
+    interface ExtendedObject {
+        id: string;
+        label: string;
+        data: any;
+        currentOwner: string;
     }
 
     let extendedAccounts: ExtendedAccount[] = $state([]);
@@ -35,6 +38,7 @@
                     address: account.address,
                     label: account.label,
                     objects: [],
+                    timelockedObjects: [],
                 };
             });
             await getObjects();
@@ -97,10 +101,73 @@
                             currentOwner: account.address,
                         };
                     });
-                    return { ...account, objects };
+
+                    // separate timelocked objects
+                    const timelockedObjects: ExtendedObject[] = [];
+                    const filteredObjects: ExtendedObject[] = [];
+                    for (const obj of objects) {
+                        if (
+                            obj.label === 'TimelockedStakedIota' ||
+                            obj.label.startsWith('TimeLock<')
+                        ) {
+                            timelockedObjects.push(obj);
+                        } else {
+                            filteredObjects.push(obj);
+                        }
+                    }
+
+                    return { ...account, objects: filteredObjects, timelockedObjects };
                 }),
             );
             extendedAccounts = updatedAccounts;
+        } catch (err: any) {
+            value = err.toString();
+            console.error(err);
+        }
+    }
+    async function dryRun() {
+        try {
+            const client = await getClient();
+            let txResults = [];
+
+            // Collect all movements
+            let movements = new Map<string, Map<string, ExtendedObject[]>>();
+            for (const account of extendedAccounts) {
+                for (const object of account.objects) {
+                    if (object.currentOwner !== account.address) {
+                        if (!movements.has(object.currentOwner)) {
+                            movements.set(object.currentOwner, new Map());
+                        }
+                        if (!movements.get(object.currentOwner)!.has(account.address)) {
+                            movements.get(object.currentOwner)!.set(account.address, []);
+                        }
+                        movements.get(object.currentOwner)!.get(account.address)!.push(object);
+                    }
+                }
+            }
+            for (const movement of movements) {
+                const senderAddress = movement[0];
+                console.log(
+                    `Moving objects from ${senderAddress} to:`,
+                    Array.from(movement[1].keys()).join(', '),
+                );
+                const tx = new Transaction();
+                for (const [to, objects] of movement[1]) {
+                    tx.transferObjects(
+                        objects.map((obj) => obj.id),
+                        to,
+                    );
+                }
+                tx.setSender(senderAddress);
+                const txBytes = await tx.build({ client });
+                // Perform a dry run
+                const dryRunResult = await client.dryRunTransactionBlock({
+                    transactionBlock: txBytes,
+                });
+                txResults.push(dryRunResult);
+            }
+
+            value = txResults;
         } catch (err: any) {
             value = err.toString();
             console.error(err);
@@ -110,6 +177,7 @@
 
 <main>
     <button onclick={syncReset}> sync/reset </button>
+    <button onclick={dryRun}> dry run </button>
 
     <JsonToggleView {value} />
 
@@ -133,17 +201,17 @@
                         <div style="border: 1px solid #525252;">
                             <div
                                 use:dragHandle
-                                aria-label="drag-handle"
                                 class="handle"
-                                style=" {account.address !== item.currentOwner
+                                style={account.address !== item.currentOwner
                                     ? 'background-color: #19400e;'
-                                    : ''}"
+                                    : ''}
                             >
                                 <span>
                                     {#if item.label.startsWith('Coin<0x2::iota::IOTA>')}
                                         {item.label}: {nanoToIota(item.data?.fields?.balance)} IOTA
                                     {:else if item.label == 'StakedIota'}
-                                        {item.label}: {nanoToIota(item.data?.fields?.principal)} IOTA
+                                        {item.label}: {nanoToIota(item.data?.fields?.principal)}
+                                        IOTA
                                     {:else if item.label == 'TimelockedStakedIota'}
                                         {item.label}: {nanoToIota(
                                             item.data.fields.staked_iota.fields.principal,
@@ -160,12 +228,13 @@
                                     </div>
                                 {/if}
                             </div>
+                            <!-- </MaybeDragHandle> -->
                             <div class="item">
                                 <details>
                                     <summary>Show object data</summary>
                                     <pre style="font-size:0.7rem;  text-align: left;">
-                                    {'\n' + JSON.stringify(item.data, null, 2)}
-                                </pre>
+                                        {'\n' + JSON.stringify(item.data, null, 2)}
+                                    </pre>
                                 </details>
                             </div>
                         </div>
@@ -175,6 +244,46 @@
                         <br />
                         <br />
                         <br />
+                    {/if}
+
+                    {#if account.timelockedObjects.length != 0}
+                        <details>
+                            <summary style="color: #ff9991;">Timelocked objects</summary>
+                            {#each account.timelockedObjects as item (item.id)}
+                                <div style="border: 1px solid #525252;">
+                                    <span style="word-break: break-all;">
+                                        {#if item.label.startsWith('Coin<0x2::iota::IOTA>')}
+                                            {item.label}: {nanoToIota(item.data?.fields?.balance)} IOTA
+                                        {:else if item.label == 'StakedIota'}
+                                            {item.label}: {nanoToIota(item.data?.fields?.principal)}
+                                            IOTA
+                                        {:else if item.label == 'TimelockedStakedIota'}
+                                            {item.label}: {nanoToIota(
+                                                item.data.fields.staked_iota.fields.principal,
+                                            )} IOTA
+                                        {:else}
+                                            {item.label}
+                                        {/if}
+                                    </span>
+                                    {#if account.address !== item.currentOwner}
+                                        <div>
+                                            from: {item.currentOwner.slice(0, 6) +
+                                                '...' +
+                                                item.currentOwner.slice(-4)}
+                                        </div>
+                                    {/if}
+                                </div>
+                                <!-- </MaybeDragHandle> -->
+                                <div class="item">
+                                    <details>
+                                        <summary>Show object data</summary>
+                                        <pre style="font-size:0.7rem;  text-align: left;">
+                                        {'\n' + JSON.stringify(item.data, null, 2)}
+                                    </pre>
+                                    </details>
+                                </div>
+                            {/each}
+                        </details>
                     {/if}
                 </div>
             </div>
