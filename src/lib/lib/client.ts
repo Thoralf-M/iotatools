aab19093140a17816e99b55efc19b62668055ff0
Thoralf-M <thoralf.mue@gmail.com>
2025-06-22 01:32:02 +0200

    feat(TextAnalyzer) add TextAnalyzer page, run prettier:fix

--- src/lib/lib/client.ts
+++ src/lib/lib/client.ts
@@ -1,9 +1,9 @@
+import { IotaClientGraphQLTransport } from '@iota/graphql-transport';
 import { IotaClient } from '@iota/iota-sdk/client';
 import { get } from 'svelte/store';
 
 import type { NetworkConfig } from './default-client-config';
 import { sharedClientConfig } from './local-storage-store';
-import { IotaClientGraphQLTransport } from '@iota/graphql-transport';
 
 // Used to determine if the client should be initialized with a new node
 let previousInitializedNodeUrl = '';
@@ -12,14 +12,12 @@ export function getClient(): IotaClient {
     let networkConfig = getSelectedNetworkConfig();
     let selectedNetworkUrl = networkConfig.node;
     if (client == undefined || selectedNetworkUrl != previousInitializedNodeUrl) {
-        client = new IotaClient(
-            {
-                transport: new IotaClientGraphQLTransport({
-                    url: networkConfig.graphql,
-                    fallbackTransportUrl: selectedNetworkUrl,
-                }),
-            }
-        );
+        client = new IotaClient({
+            transport: new IotaClientGraphQLTransport({
+                url: networkConfig.graphql,
+                fallbackTransportUrl: selectedNetworkUrl,
+            }),
+        });
         previousInitializedNodeUrl = selectedNetworkUrl;
     }
     return client;
