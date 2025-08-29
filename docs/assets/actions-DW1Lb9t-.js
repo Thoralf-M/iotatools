bed83fde472235c6204c5dadd5de9ad0821d742a
Thoralf Müller <thoralf.mue@gmail.com>
2025-08-29 21:17:25 +0200

    feat(Signer): persist external addresses and support more than one

---
+++ docs/assets/actions-DW1Lb9t-.js
@@ -0,0 +1,31 @@
+import { a7 as effect, C as untrack, ag as render_effect, x as deep_read_state, $ as safe_not_equal } from "/iota-utils/assets/index-2BfZ2Uzt.js";
+function action(dom, action2, get_value) {
+  effect(() => {
+    var payload = untrack(() => action2(dom, get_value?.()) || {});
+    if (get_value && payload?.update) {
+      var inited = false;
+      var prev = (
+        /** @type {any} */
+        {}
+      );
+      render_effect(() => {
+        var value = get_value();
+        deep_read_state(value);
+        if (inited && safe_not_equal(prev, value)) {
+          prev = value;
+          payload.update(value);
+        }
+      });
+      inited = true;
+    }
+    if (payload?.destroy) {
+      return () => (
+        /** @type {Function} */
+        payload.destroy()
+      );
+    }
+  });
+}
+export {
+  action as a
+};
