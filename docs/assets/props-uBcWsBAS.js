3cef777b969811873766d0d6465b6a8a721a0790
Thoralf Müller <thoralf.mue@gmail.com>
2025-08-31 14:41:30 +0200

    feat(Stake): add validator addresses selection

---
+++ docs/assets/props-uBcWsBAS.js
@@ -0,0 +1,13 @@
+import { aq as get_descriptor, ar as teardown } from "/iota-utils/assets/index-CoXScrZT.js";
+function bind_prop(props, prop, value) {
+  var desc = get_descriptor(props, prop);
+  if (desc && desc.set) {
+    props[prop] = value;
+    teardown(() => {
+      props[prop] = null;
+    });
+  }
+}
+export {
+  bind_prop as b
+};
