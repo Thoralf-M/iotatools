fa34312a596a5851e8dd86958f54a942e4406f70
Thoralf Müller <thoralf.mue@gmail.com>
2025-09-19 22:47:48 +0200

    feat(StakingRewards): handle inactive validators, update caches

--- src/lib/lib/staking-rewards/compute/validator-utils.ts
+++ src/lib/lib/staking-rewards/compute/validator-utils.ts
@@ -1,4 +1,6 @@
 import type { ValidatorInfo } from './types';
+import { queryDynamicField, queryDynamicFields } from '../../dynamic-fields/dynamic-fields-utils';
+import { getSelectedNetworkConfig } from '../../client';
 
 export function getCurrentActiveValidatorsExchangeRateIds(
     systemState: any,
@@ -13,6 +15,33 @@ export function getCurrentActiveValidatorsExchangeRateIds(
             validatorMap[poolId] = exchangeRateId;
         }
     }
+    return validatorMap;
+}
+
+export async function getInactiveValidatorsExchangeRateIds(
+    systemState: any,
+): Promise<Record<string, string>> {
+    const validatorMap: Record<string, string> = {};
+    if (systemState?.json?.validators?.inactive_validators.size == 0) {
+        return validatorMap;
+    }
+    const inactiveValidatorsId = systemState?.json?.validators?.inactive_validators.id;
+    let dynamicFields = await queryDynamicFields({ objectId: inactiveValidatorsId, pageSize: 50, graphqlUrl: getSelectedNetworkConfig().graphql, })
+    for (const node of dynamicFields.nodes) {
+        const result = await queryDynamicField({
+            objectId: node.value.json.inner.id,
+            fieldType: "u64",
+            bcsValue: "AQAAAAAAAAA=",
+            graphqlUrl: getSelectedNetworkConfig().graphql,
+        });
+        if (result.error) {
+            throw new Error("Failed to fetch inactive validator: " + result.error);
+        }
+        let poolId = result.field.value.json.staking_pool.id
+        let exchangeRateId = result.field.value.json.staking_pool.exchange_rates.id
+        validatorMap[poolId] = exchangeRateId;
+    }
+
     return validatorMap;
 }
 
