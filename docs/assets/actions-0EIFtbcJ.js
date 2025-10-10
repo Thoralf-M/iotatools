7c1c930c10bb5e476aa7433d9c7f64eafd3b9a2c
Thoralf Müller <thoralf.mue@gmail.com>
2025-10-10 21:19:55 +0200

    pnpm build

---
+++ docs/assets/actions-0EIFtbcJ.js
@@ -0,0 +1,31 @@
+import { a5 as effect, C as untrack, ah as render_effect, x as deep_read_state, Z as safe_not_equal } from "/iota-utils/assets/index-CyX8wKuW.js";
+function action(dom, action2, get_value) {
+  effect(() => {
+    var payload = untrack(() => action2(dom, get_value?.()) || {});
+    if (get_value && payload?.update) {
+      var inited = false;
+      var prev = (
+        /** @type {any} */
+        {}
+      );
+      render_effect(() => {
+        var value = get_value();
+        deep_read_state(value);
+        if (inited && safe_not_equal(prev, value)) {
+          prev = value;
+          payload.update(value);
+        }
+      });
+      inited = true;
+    }
+    if (payload?.destroy) {
+      return () => (
+        /** @type {Function} */
+        payload.destroy()
+      );
+    }
+  });
+}
+export {
+  action as a
+};
