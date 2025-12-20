d5139a9e11801350ad9585bc2f4e2b9447e55dbc
Thoralf Müller <thoralf.mue@gmail.com>
2025-12-20 09:42:53 +0100

    build

---
+++ docs/assets/PublishData-DPv5uVqs.js
@@ -0,0 +1,41 @@
+import { p as push, i as init, f as from_html, s as sibling, c as child, g as get, m as mutable_source, x as bind_value, h as event, k as append, l as pop, n as set, ai as Transaction } from "/iota-utils/assets/index-BLl4_Dc4.js";
+import { T as TransactionView } from "/iota-utils/assets/TransactionView-DT1zR-JM.js";
+import { e as executeTransaction } from "/iota-utils/assets/transaction-execution-CE0rGE-K.js";
+import "/iota-utils/assets/transaction-view-YEYpSRKm.js";
+import "/iota-utils/assets/explorer-links-Bx4a9wSX.js";
+import "/iota-utils/assets/client-DfgYIuj1.js";
+import "/iota-utils/assets/iota-nano-conversion-DAWjhY1n.js";
+var root = from_html(`<main>Publish data as input to a tx <br/> <span>pure input data: <input placeholder="string" size="60"/></span> <br/> <button class="svelte-ak526r">publish data in tx</button> <!></main>`);
+function PublishData($$anchor, $$props) {
+  push($$props, false);
+  let pureInputData = mutable_source("some data");
+  let value = mutable_source({});
+  const publishData = async () => {
+    try {
+      const tx = new Transaction();
+      tx.pure("string", get(pureInputData));
+      set(value, await executeTransaction(tx));
+    } catch (err) {
+      set(value, err.toString());
+      console.error(err);
+    }
+  };
+  init();
+  var main = root();
+  var span = sibling(child(main), 3);
+  var input = sibling(child(span));
+  var button = sibling(span, 4);
+  var node = sibling(button, 2);
+  TransactionView(node, {
+    get value() {
+      return get(value);
+    }
+  });
+  bind_value(input, () => get(pureInputData), ($$value) => set(pureInputData, $$value));
+  event("click", button, () => publishData());
+  append($$anchor, main);
+  pop();
+}
+export {
+  PublishData as default
+};
