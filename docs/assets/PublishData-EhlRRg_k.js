4e0b47c34269fcb741c959a2f837be22f7cb7aa1
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2026-01-08 16:12:38 +0100

    fix: use selected wallet for signing (#110)
    * fix: use selected wallet for signing
    
    * build

---
+++ docs/assets/PublishData-EhlRRg_k.js
@@ -0,0 +1,41 @@
+import { p as push, i as init, f as from_html, s as sibling, c as child, g as get, m as mutable_source, x as bind_value, h as event, k as append, l as pop, n as set, ai as Transaction } from "./index-D7gyP1pA.js";
+import { T as TransactionView } from "./TransactionView-BGrkb13t.js";
+import { e as executeTransaction } from "./transaction-execution-DdDwfMSO.js";
+import "./transaction-view-Ct4_DG2Y.js";
+import "./explorer-links-Bx4a9wSX.js";
+import "./client-wsomoPJI.js";
+import "./iota-nano-conversion-XMs16Yar.js";
+var root = from_html(`<main>Publish data as input to a tx <br/> <span>pure input data: <input placeholder="string" size="60"/></span> <br/> <button class="svelte-ak526r">publish data in tx</button> <!></main>`);
+function PublishData($$anchor, $$props) {
+  push($$props, false);
+  let pureInputData = mutable_source("some data");
+  let value = mutable_source({});
+  const publishData = async () => {
+    try {
+      const tx = new Transaction();
+      tx.pure("string", get(pureInputData));
+      set(value, await executeTransaction(tx));
+    } catch (err) {
+      set(value, err.toString());
+      console.error(err);
+    }
+  };
+  init();
+  var main = root();
+  var span = sibling(child(main), 3);
+  var input = sibling(child(span));
+  var button = sibling(span, 4);
+  var node = sibling(button, 2);
+  TransactionView(node, {
+    get value() {
+      return get(value);
+    }
+  });
+  bind_value(input, () => get(pureInputData), ($$value) => set(pureInputData, $$value));
+  event("click", button, () => publishData());
+  append($$anchor, main);
+  pop();
+}
+export {
+  PublishData as default
+};
