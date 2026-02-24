b9f1540fc8c0052740bf7abbd6bc2a6f6a940dd9
Thoralf Müller <thoralf.mue@gmail.com>
2026-02-24 11:27:04 +0100

    pnpm build

---
+++ docs/assets/PublishData-D_kUP-nL.js
@@ -0,0 +1,36 @@
+import { p as push, i as init, a as from_html, s as sibling, c as child, D as TransactionView, g as get, m as mutable_source, y as bind_value, j as event, l as append, n as pop, o as set, aD as Transaction } from "./index-FamLcqZt.js";
+import { e as executeTransaction } from "./transaction-execution-CJejU5G9.js";
+var root = from_html(`<main>Publish data as input to a tx <br/> <span>pure input data: <input placeholder="string" size="60"/></span> <br/> <button class="svelte-1dks3s0">publish data in tx</button> <!></main>`);
+function PublishData($$anchor, $$props) {
+  push($$props, false);
+  let pureInputData = mutable_source("some data");
+  let value = mutable_source({});
+  const publishData = async () => {
+    try {
+      const tx = new Transaction();
+      tx.pure("string", get(pureInputData));
+      set(value, await executeTransaction(tx));
+    } catch (err) {
+      set(value, err.toString());
+      console.error(err);
+    }
+  };
+  init();
+  var main = root();
+  var span = sibling(child(main), 3);
+  var input = sibling(child(span));
+  var button = sibling(span, 4);
+  var node = sibling(button, 2);
+  TransactionView(node, {
+    get value() {
+      return get(value);
+    }
+  });
+  bind_value(input, () => get(pureInputData), ($$value) => set(pureInputData, $$value));
+  event("click", button, () => publishData());
+  append($$anchor, main);
+  pop();
+}
+export {
+  PublishData as default
+};
