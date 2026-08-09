cb315b527b72ef90b53f4cd6292da4da2ebf520f
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2026-08-09 10:55:52 +0200

    feat(StakingRewards): update caches (#346)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- docs/assets/page-query-params-B7qI2dyb.js
+++ docs/assets/page-query-params-B7qI2dyb.js
@@ -1,6 +1,6 @@
 import { St as derived } from "./disclose-version-CpEJO7r1.js";
 import "./client-BTFoHz6u.js";
-import { r as queryParams } from "./index-k9mbAQtG.js";
+import { r as queryParams } from "./index-BPkABvMC.js";
 //#region src/lib/utils/page-query-params.ts
 /**
 * Hook for individual pages to bind form fields to query parameters
