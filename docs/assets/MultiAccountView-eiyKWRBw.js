ef8a2523bc90f9c27610acdb6f93536573c5207c
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2026-08-21 10:53:31 +0200

    feat(StakingRewards): update caches (#358)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- docs/assets/MultiAccountView-eiyKWRBw.js
+++ docs/assets/MultiAccountView-eiyKWRBw.js
@@ -2,10 +2,10 @@ import { $ as untrack, B as comment, C as bind_select_value, Ct as get$1, D as s
 import { l as IOTA_SYSTEM_STATE_OBJECT_ID, o as Transaction, t as getClient, v as sharedMultiAccountCompactAmounts, y as sharedMultiAccountCurrency } from "./client-BTFoHz6u.js";
 import { B as isValidIotaAddress } from "./keypair-DsT3ivIR.js";
 import { a as iota_accounts } from "./signer-data-D1Egmbld.js";
-import { g as nanoToIota, h as iotaToNano, n as addAndRun, p as formatNumberWithUnderscores } from "./index-eVedi1Ly.js";
-import { n as updatePageQueryParams, r as usePageQueryParams } from "./page-query-params-C6P2rEyQ.js";
+import { g as nanoToIota, h as iotaToNano, n as addAndRun, p as formatNumberWithUnderscores } from "./index-OYEMcDqs.js";
+import { n as updatePageQueryParams, r as usePageQueryParams } from "./page-query-params-CHdo1kNt.js";
 import { t as auto_default } from "./auto-DlIbMOkz.js";
-import { h as getTokenAmount, m as getIotaAmount, o as exchangeRateCache, s as fetchAllExchangeRates, t as exchange_rate_cache_default, u as setInitialExchangeRateCacheFromBinary } from "./exchange-rate-cache-aPBmrNhq.js";
+import { h as getTokenAmount, m as getIotaAmount, o as exchangeRateCache, s as fetchAllExchangeRates, t as exchange_rate_cache_default, u as setInitialExchangeRateCacheFromBinary } from "./exchange-rate-cache-Dd7nmzTS.js";
 import { t as computeStakingRewards } from "./staking-utils-Bq8uBoCa.js";
 import { t as plugin } from "./chartjs-plugin-zoom.esm-DAYqusPu.js";
 //#region node_modules/.pnpm/svelte-dnd-action@0.9.74_svelte@5.56.6/node_modules/svelte-dnd-action/dist/index.mjs
